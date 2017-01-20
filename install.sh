6c75687767fac60727cfc9fa7e6b98d15cbcf1fc
Oleksandr Komlichenko <ejastik@gmail.com>
2017-01-20 10:03:03 +0200

    id: master comment: install script updated

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
 composer install
+
 HTTPDUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1` && sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs && sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs
+
+app/console doctrine:migrations:migrate
+
+app/console cache:clear
+app/console cache:clear --env=prod
+
 sudo chmod -R 777 web/uploads
